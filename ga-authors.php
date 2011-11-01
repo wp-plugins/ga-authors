b92187e80c250800f5eead8712ecfcbf03dcf90d
mlazarov <mlazarov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-01 14:59:40 +0000

    Adding first version of GA Authors plugin
    git-svn-id: https://plugins.svn.wordpress.org/ga-authors/trunk@458290 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ga-authors.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: GA Authors
+Plugin Script: ga-authors.php
+Plugin URI: http://marto.lazarov.org/plugins/ga-authors
+Description: Track page views by authors
+Version: 1.0.1
+Author: mlazarov
+Author URI: http://marto.lazarov.org
+Min WP Version: 2.7
+Max WP Version: 3.1.2
+Update Server: http://marto.lazarov.org/plugins/ga-authors
+
+== Changelog ==
+
+= 1.0.1 =
+* Bugfix
+
+= 1.0.0 =
+* First release
+
+*/
+
+if (!class_exists('ga_authors')) {
+	class ga_authors {
+	
+		function ga_authors() {
+			$this->__construct();
+			
+		}
+		function __construct() {
+			$stored_options = get_option('ga_authors_options');
+			
+			$this->options = (array)(is_serialized($stored_options)) ? unserialize($stored_options) : $stored_options;
+			
+			// Setting filters, actions, hooks....      
+			add_action("admin_menu", array (
+				& $this,
+				"admin_menu_link"
+			));
+			
+			add_action('wp_footer', array(&$this,'footer'));
+
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* @desc Adds the options subpanel
+		*/
+		function admin_menu_link() {
+			add_management_page('GA Authors', 'GA Authors', 8, basename(__FILE__), array (
+				& $this,
+				'admin_options_page'
+			));
+			add_filter('plugin_action_links_' . plugin_basename(__FILE__), array (
+				& $this,
+				'filter_plugin_actions'
+			), 10, 2);
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* Adds the Settings link to the plugin activate/deactivate page
+		*/
+		function filter_plugin_actions($links, $file) {
+			$settings_link = '<a href="tools.php?page=' . basename(__FILE__) . '">' . __('Settings') . '</a>';
+			array_unshift($links, $settings_link); // before other links
+
+			return $links;
+		}
+		
+		function Footer(){
+			if($this->options['ga_code']){
+			?>
+
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', '<?=$this->options['ga_code'];?>']);
+	_gaq.push(['_trackPageview']);<?php 
+            if (is_single()){
+            if (have_posts()) the_post();
+        	?>
+
+	_gaq.push(['_trackPageview', '/by-author/<?=the_author_meta('display_name');?>']);<?php }?>
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+<?php
+			}
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* Administration options page
+		*/
+		function admin_options_page() {
+			global $wpdb;
+			
+			if ($_POST['ga_authors']) {
+				$this->options['ga_code'] = $_POST['ga_code'];
+				update_option('ga_authors_options', serialize($this->options));
+			
+			}
+			
+			?>
+			<div class="wrap">
+				<div id="dashboard" style="width:250px;padding:10px;">
+					<h3>GA Authors options</h3>
+					<form method="post">
+						<div  style=""> 
+							Google Analytics web property ID (UA-XXXXX-YY):<br/>
+							<input type="text" name="ga_code" value="<?=$this->options['ga_code'];?>" size="20"/>
+							<input type="submit" name="ga_authors" class="button-primary" value="Save" />
+						</div>
+					</form>
+				</div>
+				
+				
+				
+			</div>
+			<?php
+		}
+
+	} //End Class
+}
+
+if (class_exists('ga_authors')) {
+	$wp_delete_posts_var = new ga_authors();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== GA Authors ===
+Contributors: mlazarov 
+Tags: google analytics, tracking authors post views,
+Requires at least: 2.7
+Tested up to: 3.1.2
+Stable tag: 1.0.1
+
+Track page views by authors
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
